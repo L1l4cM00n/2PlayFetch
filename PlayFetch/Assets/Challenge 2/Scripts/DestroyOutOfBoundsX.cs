784605c34c0cf34d56128f4e241f62073d607bef
Lili Ribeiro <lisantos.1409@gmail.com>
2026-06-17 01:19:10 +0100

    commit

---
+++ PlayFetch/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    private float leftLimit = -30;
+    private float bottomLimit = -5;
+
+    void Update()
+    {
+        if (transform.position.x < leftLimit)
+        {
+            Destroy(gameObject);
+        } 
+        else if (transform.position.y < bottomLimit)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ PlayFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    }
+
+    void SpawnRandomBall ()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        int randomIndex = Random.Range(0, ballPrefabs.Length);
+        Instantiate(ballPrefabs[randomIndex], spawnPos, ballPrefabs[randomIndex].transform.rotation);
+    }
+}
